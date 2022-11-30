746cc9592340e52afd7b5ae5e076aff003671af9
Erin Hayes <eeh55@cam.ac.uk>
2022-11-30 09:38:26 +0000

    replace numpy with jax.numpy where easily doable

--- py/gausSN.py
+++ py/gausSN.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
+import dynesty
+import jax.numpy as jnp
 
 class GP:
     """
@@ -18,24 +20,29 @@ class GP:
         self.kernel.reset(kernel_params)
         self.meanfunc.reset(meanfunc_params)
         
-        self.cov = self.kernel.covariance(self.x, self.x) + np.diag(self.yerr**2)
+        self.cov = self.kernel.covariance(self.x, self.x) + jnp.diag(self.yerr**2)
         self.mean = self.meanfunc.mean(self.x)
         
-        a = np.log(np.linalg.det(2 * np.pi * self.cov))
-        b = np.dot(np.transpose(self.mean - self.y), np.linalg.solve(self.cov, (self.mean - self.y)))
+        a = np.log(jnp.linalg.det(2 * np.pi * self.cov))
+        b = np.dot(jnp.transpose(self.mean - self.y), jnp.linalg.solve(self.cov, (self.mean - self.y)))
         return 0.5*(a + b)
     
-    def optimize_parameters(self, x, y, yerr, bounds=None):
+    def optimize_parameters(self, x, y, yerr, method='minimize', bounds=None, ptform=None):
         
         self.ndim = len(self.kernel.params) + len(self.meanfunc.params)
         self.x = x
         self.y = y
         self.yerr = yerr
-        self.mean = self.meanfunc.mean(self.x)
         
-        init_guess = [self.kernel.params[i] for i in range(len(self.kernel.params))] + [self.meanfunc.params[i] for i in range(len(self.meanfunc.params))]
+        if method == 'minimize':
+            init_guess = [self.kernel.params[i] for i in range(len(self.kernel.params))] + [self.meanfunc.params[i] for i in range(len(self.meanfunc.params))]
+            results = minimize(self.negloglikelihood, init_guess, bounds = bounds)
         
-        results = minimize(self.negloglikelihood, init_guess, bounds = bounds)
+        if method == 'nested_sampling':
+            sampler = dynesty.NestedSampler(self.negloglikelihood, ptform, self.ndim)
+            sampler.run_nested(maxiter=20000)
+            results = sampler.results
+        
         return results
     
     def predict(self, U_x, V_x, V_y, V_yerr):
@@ -48,13 +55,13 @@ class GP:
         V_y = observed data, y
         """
         cov_UV = self.kernel.covariance(U_x, V_x)
-        cov_VV = self.kernel.covariance(V_x, V_x) + np.diag(V_yerr**2)
+        cov_VV = self.kernel.covariance(V_x, V_x) + jnp.diag(V_yerr**2)
         cov_UU = self.kernel.covariance(U_x, U_x)
 
         mu_U = self.meanfunc.mean(U_x)
         mu_V = self.meanfunc.mean(V_x)
         
-        expectation = mu_U + (cov_UV @ np.linalg.solve(cov_VV, V_y-mu_V))
-        variance = cov_UU - (cov_UV @ np.linalg.solve(cov_VV, np.transpose(cov_UV)))
+        expectation = mu_U + (cov_UV @ jnp.linalg.solve(cov_VV, V_y-mu_V))
+        variance = cov_UU - (cov_UV @ jnp.linalg.solve(cov_VV, jnp.transpose(cov_UV)))
 
         return expectation, variance
