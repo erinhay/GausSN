9a96775cfe019f774141aed61f29c1932d20c06d
Erin Hayes <53277330+erinhay@users.noreply.github.com>
2023-09-25 14:34:22 +0100

    Minor speed-ups in loglike function

--- GausSN/gausSN.py
+++ GausSN/gausSN.py
@@ -127,16 +127,17 @@ class GP:
         """
         # Compute the mean vector for the given input data points x
         self.mean = self.meanfunc.mean(x, mean_params)
+        mean_vector = self.mean - y
         
         # Compute the covariance matrix K for the given input data points x
         # and modify the covariance matrix to include magnification effects (if applicable) and measurement uncertainties
         self.cov = self.kernel.covariance(x, x, kernel_params) + jnp.diag(yerr**2)
         
         # Compute the logarithm of the determinant of the covariance matrix
-        _, a = jnp.linalg.slogdet(2 * jnp.pi * self.cov)
+        a = (len(x) * jnp.log(2 * jnp.pi)) + jnp.linalg.slogdet(self.cov)[1]
         
         # Compute the term in the exponential of the PDF of a MVN PDF
-        b = jnp.dot(jnp.transpose(self.mean - y), jnp.linalg.solve(self.cov, (self.mean - y)))
+        b = mean_vector.T @ jnp.linalg.solve(self.cov, mean_vector)
         
         # Compute the log likelihood of a MVN PDF
         loglike = -0.5*(a + b)
