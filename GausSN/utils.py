a60a4452e7a4384b5f32daa0747cf125a0cf3356
Erin Hayes <eeh55@cam.ac.uk>
2023-11-29 15:25:29 +0000

    update utils with more specific fontsize specs

--- GausSN/utils.py
+++ GausSN/utils.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use('/data/eeh55/Github/GausSN/ipynb/stylesheet/GausSN.mplstyle')
 
 def plot_object(data, color_dict={'image_1': 'darkblue', 'image_2': 'crimson', 'image_3': 'darkgreen', 'image_4': 'darkorange'}, title='Gravitationally Lensed Supernova'):
 
-    fig, ax = plt.subplots(len(np.unique(data['band'])), 1, figsize=(5,2*len(np.unique(data['band']))), sharex=True, sharey=True)
+    fig, ax = plt.subplots(len(np.unique(data['band'])), 1, figsize=(8,3*len(np.unique(data['band']))), sharex=True)
 
     if len(np.unique(data['band'])) > 1:
         ordered = np.array(['B_CSP', 'V_CSP', 'lsstu', 'lsstg', 'lsstr', 'lssti', 'lsstz', 'roman::Z', 'lssty', 'roman::Y', 'roman::J', 'roman::H', 'F105W', 'F125W', 'F160W'])
@@ -17,11 +18,11 @@ def plot_object(data, color_dict={'image_1': 'darkblue', 'image_2': 'crimson', '
                 markers, caps, bars = ax[b].errorbar(image['time'], image['flux'], yerr=image['fluxerr'], ls='None', marker='.', color=color_dict[im_id], label='Image '+im_id[-1])
                 [bar.set_alpha(0.5) for bar in bars]
             band_label = pb_id[-1] + ' band' if not np.isin(pb_id, ['F105W', 'F125W', 'F160W']) else pb_id
-            ax[b].set_ylabel(band_label)
+            ax[b].set_ylabel(band_label, fontsize=14)
 
         ax[0].legend()
-        ax[-1].set_xlabel('Time [days]')
-        ax[0].set_title(title)
+        ax[-1].set_xlabel('Time [days]', fontsize=14)
+        ax[0].set_title(title, fontsize=24)
 
     else:
         for im_id in np.unique(data['image']):
@@ -30,19 +31,19 @@ def plot_object(data, color_dict={'image_1': 'darkblue', 'image_2': 'crimson', '
             markers, caps, bars = ax.errorbar(image['time'], image['flux'], yerr=image['fluxerr'], ls='None', marker='.', color=color_dict[im_id], label='Image '+im_id[-1])
             [bar.set_alpha(0.5) for bar in bars]
         band_label = image['band'][0][-1] + ' band' if not np.isin(image['band'][0], ['F105W', 'F125W', 'F160W']) else image['band'][0]
-        ax.set_ylabel(band_label)
+        ax.set_ylabel(band_label, fontsize=14)
 
         ax.legend()
-        ax.set_xlabel('Time [days]')
-        ax.set_title(title)
+        ax.set_xlabel('Time [days]', fontsize=14)
+        ax.set_title(title, fontsize=24)
     
-    fig.supylabel('Flux', fontsize=20, x=-0.1)
+    fig.supylabel('Flux', fontsize=20, x=-0.06)
     plt.subplots_adjust(hspace=0)
     return fig
 
 def plot_object_mag(data, color_dict={'image_1': 'darkblue', 'image_2': 'crimson', 'image_3': 'darkgreen', 'image_4': 'darkorange'}, title='Gravitationally Lensed Supernova'):
 
-    fig, ax = plt.subplots(len(np.unique(data['band'])), 1, figsize=(5,2*len(np.unique(data['band']))), sharex=True)
+    fig, ax = plt.subplots(len(np.unique(data['band'])), 1, figsize=(8,3*len(np.unique(data['band']))), sharex=True)
 
     if len(np.unique(data['band'])) > 1:
         ordered = np.array(['B_CSP', 'V_CSP', 'lsstu', 'lsstg', 'lsstr', 'lssti', 'lsstz', 'roman::Z', 'lssty', 'roman::Y', 'roman::J', 'roman::H', 'F125W', 'F160W'])
@@ -54,12 +55,12 @@ def plot_object_mag(data, color_dict={'image_1': 'darkblue', 'image_2': 'crimson
                 image = band[band['image'] == im_id]
 
                 ax[b].errorbar(image['time'], image['mag'], yerr=image['magerr'], ls='None', marker='.', color=color_dict[im_id], label='Image '+im_id[-1])
-            ax[b].set_ylabel(pb_id)
+            ax[b].set_ylabel(pb_id, fontsize=14)
             ax[b].invert_yaxis()
 
         ax[0].legend()
-        ax[-1].set_xlabel('MJD [days]')
-        ax[0].set_title(title)
+        ax[-1].set_xlabel('MJD [days]', fontsize=14)
+        ax[0].set_title(title, fontsize=24)
 
     else:
         for im_id in np.unique(data['image']):
@@ -70,9 +71,9 @@ def plot_object_mag(data, color_dict={'image_1': 'darkblue', 'image_2': 'crimson
         ax.invert_yaxis()
 
         ax.legend()
-        ax.set_xlabel('MJD')
-        ax.set_title(title)
+        ax.set_xlabel('MJD', fontsize=14)
+        ax.set_title(title, fontsize=24)
     
-    fig.supylabel('Magnitude', fontsize=20, x=-0.1)
+    fig.supylabel('Magnitude', fontsize=20, x=-0.06)
     plt.subplots_adjust(hspace=0)
     return fig
