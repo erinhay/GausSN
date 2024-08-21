27ab90e81b07f09a06a9217e442bc8fa276387c5
Erin Hayes <eeh55@cam.ac.uk>
2024-08-21 13:19:02 +0100

    lowercase gaussn

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='GausSN',
+    name='gaussn',
     version='0.1.0',    
     description='A package for fitting gravitationally lensed supernova light curves and other astronomical data with Gaussian Processes.',
     url='https://github.com/erinhay/GausSN',
     author='Erin Hayes',
     author_email='eeh55@cam.ac.uk',
-    packages=['GausSN'],
+    packages=['gaussn'],
     install_requires=['numpy', 'jax', 'matplotlib'],
     classifiers=[
         'Development Status :: 1 - Planning',
