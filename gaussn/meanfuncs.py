d9ac2c065a3b0407b018ed9490cb27463e70d785
Erin Hayes <eeh55@cam.ac.uk>
2026-09-08 20:27:54 +0100

    update to xprime is none everywhere, change np to jnp in mean funcs, change for loops in villar2019 to jnp where, fix missing exponential in rational quadratic kernel

--- gaussn/meanfuncs.py
+++ gaussn/meanfuncs.py
@@ -96,7 +96,7 @@ class Sin(BaseMeanFunction):
         """
         if params != None:
             self._reset(params)
-        return self.A * np.sin((y*self.w) + self.phi)
+        return self.A * jnp.sin((y*self.w) + self.phi)
     
 class Gaussian(BaseMeanFunction):
     """
@@ -161,8 +161,8 @@ class ExpFunction(BaseMeanFunction):
         """
         if params != None:
             self._reset(params)
-        mu = np.zeros([len(y)])
-        mu = self.A * np.exp(y*self.tau)
+        mu = jnp.zeros([len(y)])
+        mu = self.A * jnp.exp(y*self.tau)
         return mu
 
 class Bazin2009(BaseMeanFunction):
@@ -200,10 +200,10 @@ class Bazin2009(BaseMeanFunction):
         """
         if params != None:
             self._reset(params)
-        mu = np.zeros([len(y)])
+        mu = jnp.zeros([len(y)])
 
-        a = np.exp(-(y - self.t0)/self.Tfall)
-        b = 1 + np.exp((y - self.t0)/self.Trise)
+        a = jnp.exp(-(y - self.t0)/self.Tfall)
+        b = 1 + jnp.exp((y - self.t0)/self.Trise)
         mu = self.A * (a/b) + self.beta
         return mu
 
@@ -223,8 +223,8 @@ class Karpenka2012(BaseMeanFunction):
                 Trise (float): Rise timescale parameter of the Karpenka function.
                 offset (float): Offset parameter of the Karpenka function.
         """
-        self.A = np.exp(params[0])
-        self.B = np.exp(params[1])
+        self.A = jnp.exp(params[0])
+        self.B = jnp.exp(params[1])
         self.t1 = params[2]
         self.t0 = params[3]
         self.Tfall = params[4]
@@ -245,11 +245,11 @@ class Karpenka2012(BaseMeanFunction):
         """
         if params != None:
             self._reset(params)
-        mu = np.zeros([len(y)])
+        mu = jnp.zeros([len(y)])
 
         a = 1 + (self.B * ((y - self.t1)**2))
-        b = np.exp(-(y - self.t0)/self.Tfall)
-        c = 1 + np.exp(-(y - self.t0)/self.Trise)
+        b = jnp.exp(-(y - self.t0)/self.Tfall)
+        c = 1 + jnp.exp(-(y - self.t0)/self.Trise)
         mu = (self.A * a * (b/c))
         return mu
 
@@ -290,19 +290,13 @@ class Villar2019(BaseMeanFunction):
         """
         if params != None:
             self._reset(params)
-        mu = np.zeros([len(y)])
-        denom = 1 + np.exp(-(y - self.t0)/self.Trise)
+        mu = jnp.zeros([len(y)])
+        denom = 1 + jnp.exp(-(y - self.t0)/self.Trise)
         constant = self.A + (self.beta * (self.t1 - self.t0))
-    
-        for i in range(len(y)):
-            if y[i] < self.t1:
-                a = self.A + (self.beta * (y[i] - self.t0))
-                mu[i] = a
-            else:
-                b = np.exp(-(y[i] - self.t1)/self.Tfall)
-                mu[i] = (constant*b)
-                
-        mu = (mu/denom)
+
+        a = self.A + (self.beta * (y - self.t0))
+        b = jnp.exp(-(y - self.t1)/self.Tfall)
+        mu = jnp.where(y<self.t1, a, constant*b) / denom
         return mu
 
 
@@ -342,7 +336,7 @@ class sncosmoMean:
             pass
         self.model.update(params_dict)
 
-    def mean(self, x, params=None, bands=None, images=None, zp=27.5, zpsys='ab'):
+    def mean(self, y, params=None, bands=None, images=None, zp=27.5, zpsys='ab'):
         """
         Computes the mean flux using the sncosmo model.
 
@@ -358,11 +352,11 @@ class sncosmoMean:
         if params != None:
             self._reset(params)
 
-        args = np.argsort(x)
+        args = np.argsort(y)
         revert_args = np.zeros(len(args), dtype=int)
         revert_args[args] = np.arange(len(args))
 
-        reordered_x = x[args]
+        reordered_y = y[args]
         if len(bands) >= 2:
             reordered_bands = bands[args]
         else:
@@ -378,7 +372,7 @@ class sncosmoMean:
         else:
             reordered_zpsys = zpsys
 
-        flux = self.model.bandflux(reordered_bands, reordered_x, zp=reordered_zp, zpsys=reordered_zpsys)
+        flux = self.model.bandflux(reordered_bands, reordered_y, zp=reordered_zp, zpsys=reordered_zpsys)
 
         return flux[revert_args]
 
